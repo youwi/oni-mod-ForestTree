27be5a61c9541ec2fc39608296451aa0a152531c
yu <yu@sec.com>
2022-10-05 04:57:22 +0800

    强制添加变异信息

--- Class1.cs
+++ Class1.cs
@@ -9,49 +9,99 @@ using UnityEngine;
 
 namespace oni_ForestTree
 {
- 
+
     [HarmonyPatch(typeof(ForestTreeBranchConfig))]
     public class ForestTreeBranchConfigPatch
     {
+
         //ForestTreeBranchConfig
         //参考: https://bbs.3dmgame.com/thread-6333693-1-1.html
         //收获种子可以得到变异属性的种子.
-        [HarmonyPatch("CreatePrefab")]
-        public static GameObject Postfix(GameObject __result)
+        [HarmonyPatch("OnSpawn")]
+        public static void Postfix(GameObject inst,GameObject __instance)
         {
-            GameObject gameObject = __result;
+            GameObject gameObject = inst;
             EntityTemplates.CreateAndRegisterPreviewForPlant(
                 EntityTemplates.CreateAndRegisterSeedForPlant(gameObject,
                 SeedProducer.ProductionType.Harvest, "ForestTreeSeed",
-                STRINGS.CREATURES.SPECIES.SEEDS.WOOD_TREE.NAME, STRINGS.CREATURES.SPECIES.SEEDS.WOOD_TREE.DESC,
+                STRINGS.CREATURES.SPECIES.SEEDS.WOOD_TREE.NAME,
+                STRINGS.CREATURES.SPECIES.SEEDS.WOOD_TREE.DESC,
                 Assets.GetAnim("seed_tree_kanim"), "object", 1,
-                new List<Tag>
-                {
-                    GameTags.CropSeed
-                }, SingleEntityReceptacle.ReceptacleDirection.Top, default(Tag), 4, STRINGS.CREATURES.SPECIES.WOOD_TREE.DOMESTICATEDDESC, EntityTemplates.CollisionShape.CIRCLE, 0.3f, 0.3f, null, "", false), "ForestTree_preview", Assets.GetAnim("tree_kanim"), "place", 3, 3);
+                new List<Tag> { GameTags.CropSeed },
+                SingleEntityReceptacle.ReceptacleDirection.Top, default(Tag), 4,
+                STRINGS.CREATURES.SPECIES.WOOD_TREE.DOMESTICATEDDESC,
+                EntityTemplates.CollisionShape.CIRCLE, 0.3f, 0.3f, null, "", false),
+            "ForestTree_preview", Assets.GetAnim("tree_kanim"),
+            "place", 3, 3);
             //SeedProducer.ProductionType.Harvest//收获掉落
 
 
-           // var mutations=gameObject.GetComponent<MutantPlant>();//获取变异属性
+            // var mutations=gameObject.GetComponent<MutantPlant>();//获取变异属性
             //生成的树枝需要从上级继承才行.
-          //  gameObject.GetComponentInParent<MutantPlant>();//获取上级的变异属性.
+            //  gameObject.GetComponentInParent<MutantPlant>();//获取上级的变异属性.
 
-            KPrefabID component2 = gameObject.GetComponent<KPrefabID>();
-            MutantPlant seed= gameObject.GetComponent<MutantPlant>();
-            if (seed != null)
-            {
-                Debug.LogWarning("树枝生成时：CreatePrefab上级变异信息为：" + seed.MutationIDs);
-            }
-          
+            MutantPlant seedParent = gameObject.GetComponentInParent<MutantPlant>();
+            MutantPlant seed = gameObject.GetComponent<MutantPlant>();
 
-            if (component2 != null)
+            if (seedParent != null)
             {
-                gameObject.AddOrGet<MutantPlant>().SpeciesID = component2.PrefabTag;//上级如果是种子的形态.
-                Debug.LogWarning("树枝生成时：PrefabTag：" + seed.MutationIDs);
-                // gameObject.AddOrGet<MutantPlant>().SpeciesID = component2.SpeciesID;上级如果是有ID,是树枝的静态.
+                Console.WriteLine("树枝上级生成时：GetComponentInParent<MutantPlant>：" + seedParent.MutationIDs);
             }
-            return gameObject;
+
+            // seed.SpeciesID = component2.PrefabTag;//上级如果是种子的形态.
+
+            // gameObject.AddOrGet<MutantPlant>().SpeciesID = component2.SpeciesID;上级如果是有ID,是树枝的静态.
+
+            // seed.Mutate();//获取随机变异. 
+
+            seed.SpeciesID = new Tag("rottenHeaps");//强制修改为旺盛属性.
+            seed.ApplyMutations();
+            seed.AddTag(GameTags.MutatedSeed);
+            seed.AddTag(new Tag("rottenHeaps"));//上面的办法改不了?
+
+
+            Components.MutantPlants.Add(seed);
+            List<string> list =  new List<string>();
+            list.Add(Db.Get().PlantMutations.rottenHeaps.Id);
+            seed.Analyze();
+            seed.SetSubSpecies(list);
+            seed.ApplyMutations();
+            // PlantSubSpeciesCatalog.Instance.DiscoverSubSpecies(GetSubSpeciesInfo(), seed);
+
+
+            Console.WriteLine("ForestTreeBranchConfig树枝生成时：seed.MutationIDs：" + seed.MutationIDs);
+            Console.WriteLine("ForestTreeBranchConfig树枝生成时：seed.IsOriginal：" + seed.IsOriginal);
+            
+          //  return gameObject;
         }
 
     }
+
+    [HarmonyPatch(typeof(BuddingTrunk))]
+    public class BuddingTrunkPatch
+    {
+
+        // Tree-->BuddingTrunk x6--TreeBudx6---TreeBranch
+        // 树---对应6个节点
+        [HarmonyPatch("OnSpawn")]
+        public static void Postfix(BuddingTrunk __instance)
+        {
+            var gameObject = __instance.GetComponentInParent<MutantPlant>();
+            Console.WriteLine("BuddingTrunk 测试信息：" + gameObject.MutationIDs);
+        }
+    }
+    [HarmonyPatch(typeof(TreeBud))]
+    public class BTreeBudPatch
+    {
+
+        // Tree-->BuddingTrunk x6--TreeBudx6---TreeBranch
+        // 树---对应6个节点
+        [HarmonyPatch("OnSpawn")]
+        public static void Postfix(TreeBud __instance)
+        {
+            var gameObject = __instance.GetComponentInParent<MutantPlant>();
+            Console.WriteLine("BTreeBudPatch 测试信息：" + gameObject.MutationIDs);
+        }
+    }
 }
+
