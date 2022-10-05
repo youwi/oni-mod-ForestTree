a3920e4833a3562a97215c0808df66e3ff07de4d
yu <yu@sec.com>
2022-10-05 10:58:40 +0800

    树枝变异成功.不过显示信息还有问题.不处理了

--- Class1.cs
+++ Class1.cs
@@ -3,6 +3,7 @@ using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -39,21 +40,24 @@ namespace oni_ForestTree
             // var mutations=gameObject.GetComponent<MutantPlant>();//获取变异属性
             //生成的树枝需要从上级继承才行.
             //  gameObject.GetComponentInParent<MutantPlant>();//获取上级的变异属性.
-
+            // Tree-->BuddingTrunk x6--TreeBudx6---TreeBranch
+            //MutantPlant seedParent =  gameObject.GetComponentInParent<TreeBud>().GetComponentInParent<BuddingTrunk>().GetComponentInParent<MutantPlant>();
+            //MutantPlant seedParent =  gameObject.FindComponent<TreeBud>().FindComponent<BuddingTrunk>().GetComponentInParent<MutantPlant>();
             MutantPlant seedParent = gameObject.GetComponentInParent<MutantPlant>();
-            MutantPlant seed = gameObject.GetComponent<MutantPlant>();
 
             if (seedParent != null)
             {
-                Console.WriteLine("树枝上级生成时：GetComponentInParent<MutantPlant>：" + seedParent.MutationIDs);
+                Console.WriteLine("ForestTreeBranchConfig树枝上级生成时：GetComponentInParent<MutantPlant>：" + seedParent.SpeciesID);
             }
+
+            MutantPlant seed = gameObject.GetComponent<MutantPlant>();
 
             // seed.SpeciesID = component2.PrefabTag;//上级如果是种子的形态.
 
             // gameObject.AddOrGet<MutantPlant>().SpeciesID = component2.SpeciesID;上级如果是有ID,是树枝的静态.
 
             // seed.Mutate();//获取随机变异. 
-
+/*
             seed.SpeciesID = new Tag("rottenHeaps");//强制修改为旺盛属性.
             seed.ApplyMutations();
             seed.AddTag(GameTags.MutatedSeed);
@@ -67,10 +71,10 @@ namespace oni_ForestTree
             seed.SetSubSpecies(list);
             seed.ApplyMutations();
             // PlantSubSpeciesCatalog.Instance.DiscoverSubSpecies(GetSubSpeciesInfo(), seed);
+*/
 
-
-            Console.WriteLine("ForestTreeBranchConfig树枝生成时：seed.MutationIDs：" + seed.MutationIDs);
-            Console.WriteLine("ForestTreeBranchConfig树枝生成时：seed.IsOriginal：" + seed.IsOriginal);
+            //Console.WriteLine("ForestTreeBranchConfig树枝生成时：seed.MutationIDs：" + seed.SpeciesID);
+           // Console.WriteLine("ForestTreeBranchConfig树枝生成时：seed.IsOriginal：" + seed.IsOriginal);
             
           //  return gameObject;
         }
@@ -82,12 +86,16 @@ namespace oni_ForestTree
     {
 
         // Tree-->BuddingTrunk x6--TreeBudx6---TreeBranch
-        // 树---对应6个节点
+        // 树---对应6个节点,
         [HarmonyPatch("OnSpawn")]
         public static void Postfix(BuddingTrunk __instance)
         {
             var gameObject = __instance.GetComponentInParent<MutantPlant>();
-            Console.WriteLine("BuddingTrunk 测试信息：" + gameObject.MutationIDs);
+            //Console.WriteLine("BuddingTrunk 测试变异信息：" + gameObject.SpeciesID); //结果是 ForestTree
+            /*  __instance.AddTag(GameTags.MutatedSeed);
+              __instance.FindOrAddComponent<MutantPlant>();*/
+         
+           
         }
     }
     [HarmonyPatch(typeof(TreeBud))]
@@ -96,11 +104,18 @@ namespace oni_ForestTree
 
         // Tree-->BuddingTrunk x6--TreeBudx6---TreeBranch
         // 树---对应6个节点
+        // 从这里复制变异信息
         [HarmonyPatch("OnSpawn")]
         public static void Postfix(TreeBud __instance)
         {
-            var gameObject = __instance.GetComponentInParent<MutantPlant>();
-            Console.WriteLine("BTreeBudPatch 测试信息：" + gameObject.MutationIDs);
+            var mutantBranch = __instance.GetComponentInParent<MutantPlant>();
+           // Console.WriteLine("BTreeBudPatch 测试变异信息：" + mutantBranch.SpeciesID);//结果是 ForestTreeBranch
+            var mutantUp= __instance.buddingTrunk.Get().GetComponentInParent<MutantPlant>();
+
+            mutantUp.CopyMutationsTo(mutantBranch);
+            mutantBranch.ApplyMutations();
+            // GetComponent<KSelectable>().SetName(GetSubSpeciesInfo().GetNameWithMutations(component.PrefabTag.ProperName(), flag, flag2));
+            // 可以手动更新名字“乔木（旺盛）”需要手动更新信息。这里太复杂了,不做.
         }
     }
 }
